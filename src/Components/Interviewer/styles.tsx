06deb96a4680deb2e9dc81200cc2843c1fb0c577
DavidSouza-Dev <davidrs.souza@gmail.com>
2022-05-29 11:43:17 -0300

    feat: implementação page interviewer e inclusão da jobApplicationList

---
+++ src/Components/Interviewer/styles.tsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+export const Wrapper = styled.section`
+  display: flex;
+  justify-content: space-between;
+  align-items: flex-start;
+  margin: auto;
+  width: 56.25rem;
+  padding: 3rem 0 0 0;
+`
+export const Container = styled.div`
+  width: 400px;
+`
