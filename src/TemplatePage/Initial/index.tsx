44f6f080ecad4205db7a16c39dc99af708d7c6dc
DavidSouza-Dev <davidrs.souza@gmail.com>
2022-05-30 16:54:26 -0300

    feat: Implementação Initial Page

---
+++ src/TemplatePage/Initial/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import * as S from './styles'
+
+const Initial = () => {
+  return (
+    <S.Wrapper>
+      <S.Heading>Tela Inicial</S.Heading>
+      <S.Title>Escolha uma tela e realize uma ação</S.Title>
+    </S.Wrapper>
+  )
+}
+
+export default Initial
---
+++ src/TemplatePage/Initial/styles.tsx
@@ -0,0 +1,28 @@
+import styled from 'styled-components'
+
+export const Wrapper = styled.section`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  flex-wrap: wrap;
+  margin: 4rem auto auto auto;
+  width: 30rem;
+  height: 10rem;
+  padding: 0.8rem;
+  box-shadow: 0px 3px 6px var(--borderColor);
+  border-radius: 10px;
+`
+export const Heading = styled.h2`
+  margin-bottom: 1rem;
+  text-align: center;
+`
+
+export const Title = styled.h3`
+  margin-bottom: 1rem;
+  width: 100%;
+  text-align: center;
+`
+export const Container = styled.div`
+  width: 25rem;
+`
