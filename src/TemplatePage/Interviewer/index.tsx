fe922650c8e3d898b37f1d804ea35517b2c01608
DavidSouza-Dev <davidrs.souza@gmail.com>
2022-05-30 21:17:15 -0300

    chore: remoção de comentários

--- src/TemplatePage/Interviewer/index.tsx
+++ src/TemplatePage/Interviewer/index.tsx
@@ -1,12 +1,10 @@
 import React from 'react'
 import JobApplicationList from '../../Components/JobApplicationList'
 import ListingJobs from '../../Components/ListingJobs'
-// import { Context } from '../../provider/context'
+
 import * as S from './styles'
 
 const Interviewer = () => {
-  // const { getAllJobs } = useContext(Context)
-
   return (
     <S.Wrapper>
       <S.Title>Área do Entrevistador</S.Title>
