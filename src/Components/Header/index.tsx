d22a741ce30da003b24d4886f3a8698fb6487cc0
DavidSouza-Dev <davidrs.souza@gmail.com>
2022-05-30 21:12:39 -0300

    refactor: remoção da implementação do ContextAPI

--- src/Components/Header/index.tsx
+++ src/Components/Header/index.tsx
@@ -1,23 +1,17 @@
-import { useContext } from 'react'
 import { Link } from 'react-router-dom'
-import { Context } from '../../provider/context'
 import { Container, Content } from './styles'
 
 const Header = () => {
-  const { recruiterPage, candidatePage, interviewerPage } = useContext(Context)
   return (
     <Container>
       <Content>
-        <Link className={`link ${candidatePage && 'active'}`} to="/Candidato">
+        <Link className="link" to="/Candidato">
           Candidato
         </Link>
-        <Link
-          className={`link ${interviewerPage && 'active'}`}
-          to="/Entrevistador"
-        >
+        <Link className="link" to="/Entrevistador">
           Entrevistador
         </Link>
-        <Link className={`link ${recruiterPage && 'active'}`} to="/Recrutador">
+        <Link className="link" to="/Recrutador">
           Recrutador
         </Link>
       </Content>
