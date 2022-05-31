cbc0519c183214f824c831f762b6446d8d7c0a13
DavidSouza-Dev <davidrs.souza@gmail.com>
2022-05-30 21:19:22 -0300

    style: inserção de estilos

--- src/Components/ListingJobs/styles.tsx
+++ src/Components/ListingJobs/styles.tsx
@@ -5,6 +5,7 @@ export const Container = styled.div`
   padding: 0.8rem;
   box-shadow: 0px 3px 6px var(--borderColor);
   border-radius: 10px;
+
   .job-item {
     background: #f4f3f6;
     margin: 0.2rem 0;
@@ -18,5 +19,21 @@ export const Container = styled.div`
     &--status {
       font-size: 0.75rem;
     }
+  }
+  p {
+    margin: auto 0 auto auto;
+    background: var(--blue);
+    border-radius: 25px;
+    height: 30px;
+    padding: 0.3rem 0.8rem;
+    font-size: 0.75rem;
+    color: white;
+    font-weight: bold;
+    transition: all 0.3s ease 0s;
+    outline: none;
+    border: none;
+    opacity: 0.7;
+    display: flex;
+    align-items: center;
   }
 `
