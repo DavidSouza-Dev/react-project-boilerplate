3f41eaa84861c20395f78b964f4703a254c2b14a
david.souza <david.pereira@gupy.com.br>
2024-12-08 12:20:01 -0300

    feat: boilerplate react-project

--- src/Routes/routes.tsx
+++ src/Routes/routes.tsx
@@ -1,21 +1,13 @@
 import React from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Header from '../Components/Header'
-import Candidate from '../TemplatePage/Candidate'
-import Initial from '../TemplatePage/Initial'
-import Interviewer from '../TemplatePage/Interviewer'
-import Recruiter from '../TemplatePage/Recruiter'
+import { HashRouter, Route, Routes } from 'react-router-dom'
+import Home from '../Home'
 const Router = () => {
   return (
-    <BrowserRouter>
-      <Header />
+    <HashRouter basename="/">
       <Routes>
-        <Route element={<Initial />} path="/" />
-        <Route element={<Candidate />} path="/Candidato" />
-        <Route element={<Interviewer />} path="/Entrevistador" />
-        <Route element={<Recruiter />} path="/Recrutador" />
+        <Route element={<Home />} path="/" />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
 
---
+++ src/__tests__/Components/test.test.jsx
@@ -0,0 +1,5 @@
+describe('simple test', () => {
+  it('should render app', () => {
+    expect(1 + 1).toBe(2)
+  })
+})
