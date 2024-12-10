5702d80ba22043cb4895fbfa53ce9bd08f7702c2
David Souza <david.pereira@gupy.com.br>
2024-12-10 15:52:27 -0300

    Update routes.tsx

--- src/Routes/routes.tsx
+++ src/Routes/routes.tsx
@@ -3,7 +3,7 @@ import { HashRouter, Route, Routes } from 'react-router-dom'
 import Home from '../Home'
 const Router = () => {
   return (
-    <HashRouter basename={process.env.PUBLIC_URL}>
+    <HashRouter basename="/">
       <Routes>
         <Route element={<Home />} path="/" />
       </Routes>
